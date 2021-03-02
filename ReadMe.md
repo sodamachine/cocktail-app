2b8bfc16f78ba344b0566a2848f9194d0bf224b5
Stephanie <stephanie.ohn@gmail.com>
2021-03-02 18:21:06 -0500

    adds prompt_for_ingredient in cli

--- ReadMe.md
+++ ReadMe.md
@@ -1,2 +1,2 @@
 'api_key = "1"'
-'url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"' 
+'url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka"' 
--- lib/cli.rb
+++ lib/cli.rb
@@ -2,8 +2,15 @@ class Cli
 
     def welcome
         puts "Welcome to the cocktail cli app!"
+        prompt_for_ingredient #call method on instance (implied: self.prompt_for_ingredient)
     end
 
+    def prompt_for_ingredient
+        puts "Please enter a cocktail ingredient."
+        input = gets.strip #rid of potential inputted white space
+        #do something with the ingredient
+        binding.pry
+    end
 
 
 end
