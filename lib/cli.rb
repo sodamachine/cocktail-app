7da077b82296b9770362a6dd516116c41e525fa7
Stephanie <stephanie.ohn@gmail.com>
2021-03-03 13:48:47 -0500

    adds list of options for user to select based on input

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,7 +9,14 @@ class Cli
         puts "Please enter the name of a cocktail you'd like to learn more about."
         input = gets.strip
         new_cocktail = Api.get_cocktail_by_name(input)
+        self.cocktail_options(new_cocktail)
     end
 
+    def cocktail_options(cocktail)
+        puts "Choose from the following list for more details about #{cocktail.name}s."
+        puts "1. Proper glass"
+        puts "2. Ingredients"
+        puts "3. Instructions"
+    end
     
 end
