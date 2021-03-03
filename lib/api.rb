133a023e52e950039152d02fe9eaaa1b2e21743a
Stephanie <stephanie.ohn@gmail.com>
2021-03-03 13:35:41 -0500

    makes api get_cocktail_by_name dynamic

--- lib/api.rb
+++ lib/api.rb
@@ -3,7 +3,7 @@ class Api
     @@api_key = "1"
     @@url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"  
 
-    def self.get_cocktail_by_name
+    def self.get_cocktail_by_name(name)
         response = HTTParty.get(@@url)
         cocktail_hash = {cocktail_hash = {name: response["drinks"][0]["strDrink"], glass: response["drinks”][0][“strGlass"], instructions: response["drinks"][0]["strInstructions"]}
         Cocktail.new(cocktail_hash)
--- lib/cli.rb
+++ lib/cli.rb
@@ -2,14 +2,13 @@ class Cli
 
     def welcome
         puts "Welcome to the cocktail cli app!"
-        prompt_for_cocktail #call method on instance (implied: self.prompt_for_ingredient)
+        prompt_for_cocktail
     end
 
     def prompt_for_cocktail
         puts "Please enter the name of a cocktail you'd like to learn more about."
-        input = gets.strip #rid of potential inputted white space
-        #do something with the ingredient
-        Api.get_cocktail_by_name
+        input = gets.strip
+        new_cocktail = Api.get_cocktail_by_name(input)
     end
     
 end
