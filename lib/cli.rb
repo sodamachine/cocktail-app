0865c81b526393829a40acedd6e636b8efc77955
Stephanie <stephanie.ohn@gmail.com>
2021-03-03 16:02:11 -0500

    adds responses based on user's selection

--- lib/cli.rb
+++ lib/cli.rb
@@ -17,6 +17,21 @@ class Cli
         puts "1. Proper glass"
         puts "2. Ingredients"
         puts "3. Instructions"
+        self.user_selection(cocktail)
     end
-    
+
+    def user_selection(cocktail)
+        input = gets.strip
+        if input == "1" || input.include?("Proper") || input.include?("proper") || input.include?("Glass") || input.include?("glass")
+            puts "The proper glass for #{cocktail.name}s is a #{cocktail.glass}."
+        elsif input == "2" || input.include?("Ingredients") || input.include?("ingredients")
+            puts "The ingredients necessary to make a #{cocktail.name} include: #{}."
+        elsif input == "3" || input.include?("Instructions") || input.include?("instructions")
+            puts "Instructions to make a #{cocktail.name} are as follows: #{cocktail.instructions}."
+        else 
+            puts "Your selection was invalid."
+            self.cocktail_options(cocktail)
+        end
+    end 
+
 end
