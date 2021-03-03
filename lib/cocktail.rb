b99d895137230babc3186119d0a9fc7d2dbb979b
Stephanie <stephanie.ohn@gmail.com>
2021-03-03 13:25:20 -0500

    mass-assign parsed data as attributes in Cocktail

--- lib/cocktail.rb
+++ lib/cocktail.rb
@@ -1,4 +1,10 @@
 class Cocktail
     attr_accessor :name, :glass, :instructions
 
+    def initialize(hash)
+        hash.each do |key, value|
+            self.send("#{key}=", value) if self.respond_to?("#{key}=")
+        end
+    end
+
 end
