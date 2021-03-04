e039903656d234402fd9cc40f137237d6f486e1a
Stephanie <stephanie.ohn@gmail.com>
2021-03-04 00:22:49 -0500

    includes error handling and user options to navigate the app or exit

--- lib/api.rb
+++ lib/api.rb
@@ -3,6 +3,7 @@ class Api
     def self.get_cocktails_by_name(input)
         url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{input}"
         response = HTTParty.get(url)["drinks"] #array
+        return nil if response == nil
         response.each{|cocktail| Cocktail.new(cocktail)}
     end
 
--- lib/cli.rb
+++ lib/cli.rb
@@ -2,14 +2,19 @@ class Cli
 
     def welcome
         puts "Welcome to the cocktail cli app!"
-        prompt_for_cocktail
+        self.prompt_for_cocktail
     end
 
     def prompt_for_cocktail
-        puts "Please enter the name of a cocktail you'd like to learn more about."
+        puts "Please enter a keyword and/or name of a cocktail you'd like to learn more about."
         input = gets.strip
-        Api.get_cocktails_by_name(input)
-        self.cocktail_options
+        cocktails = Api.get_cocktails_by_name(input)
+        if cocktails
+            self.cocktail_options
+        else
+            puts "Your selection was invalid."
+            self.prompt_for_cocktail
+        end
     end
 
     def cocktail_options
@@ -30,25 +35,43 @@ class Cli
         puts "FYI: Select what information you'd like to find out about the #{cocktail.strDrink}."
         puts "1. Glass"
         puts "2. Category"
-        puts "2. Instructions"
+        puts "3. Instructions"
         self.user_selects_info(cocktail)
     end
 
     def user_selects_info(cocktail)
         input = gets.strip
-        a = ["1. Glass", "1", "Glass", "glass"]
-        b = ["2. Category", "2", "Category", "category"]
-        c = ["3. Instructions", "3", "Instructions", "instructions"]
-        if a.include?(input)
+        if input == "1" || input.downcase.include?("glass")
             puts "The proper glass for a #{cocktail.strDrink} is a #{cocktail.strGlass}."
-        elsif b.include?(input)
+        elsif input == "2" || input.downcase.include?("category")
             puts "The #{cocktail.strDrink} is part of the #{cocktail.strCategory} category."
-        elsif c.include?(input)
+        elsif input == "3" || input.downcase.include?("instructions")
             puts "Instructions to make a #{cocktail.strDrink} are as follows: #{cocktail.strInstructions}."
         else 
             puts "Your selection was invalid."
-            self.prompt_for_cocktail
         end
+        self.more_options(cocktail)
     end
 
+    def more_options(cocktail)
+        puts "Type 'back' to return to the list of options for the #{cocktail.strDrink}."
+        puts "Type 'more' to see other cocktails from your search results."
+        puts "Type 'new' if you'd like to search for a new cocktail."
+        puts "Type 'exit' to exit the application."
+        self.user_selects_option(cocktail)
+    end
+
+    def user_selects_option(cocktail)
+        input = gets.strip
+        if input.downcase == "back"
+            self.info_options(cocktail)
+        elsif input.downcase == "more"
+            self.cocktail_options
+        elsif input.downcase == "new"
+            self.prompt_for_cocktail
+        else
+            puts "Goodbye!"
+            return
+        end
+    end
 end
